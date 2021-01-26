3507f8c21735020aa99ab16aa5b98a388911e389
rockts <rockts@sina.com>
2021-01-27 03:55:47 +0800

    添加默认图片显示

--- src/components/CategoryList.vue
+++ src/components/CategoryList.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="row">
-    <div v-for="category in list" :key="category.id" class="col-md-4 mb-4">
+    <div
+      v-for="category in categoryList"
+      :key="category.id"
+      class="col-md-4 mb-4"
+    >
       <div class="card h-100 shadow-sm">
         <div class="card-body text-center">
           <img
-            :src="category.avatar"
+            :src="category.cover"
             :alt="category.name"
-            class="card-img-top"
+            class="card-img-top w-50"
           />
           <h5 class="card-title mt-4">{{ category.name }}</h5>
           <p class="card-text">{{ category.alias }}</p>
@@ -18,10 +22,10 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from 'vue';
+import { computed, defineComponent, PropType } from 'vue';
 export interface CategoryProps {
   id: number;
-  avatar: string;
+  cover?: string;
   name: string;
   alias: string;
 }
@@ -35,7 +39,15 @@ export default defineComponent({
     },
   },
   setup(props) {
-    props;
+    const categoryList = computed(() => {
+      return props.list.map((category) => {
+        if (!category.cover) {
+          category.cover = require('@/assets/catagory.png');
+        }
+        return category;
+      });
+    });
+    return { categoryList };
   },
 });
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -17,37 +17,37 @@ const testData: CategoryProps[] = [
     id: 1,
     name: '教材',
     alias: 'textbook',
-    avatar: 'http://localhost:8080/img/res_cover.82160bf0.png',
+    cover: require('@/assets/catagory.png'),
   },
   {
     id: 2,
     name: '课件',
     alias: 'coursewarel',
-    avatar: 'http://localhost:8080/img/res_cover.82160bf0.png',
+    cover: require('@/assets/catagory.png'),
   },
   {
     id: 3,
     name: '同步练习',
     alias: 'questions',
-    avatar: 'http://localhost:8080/img/res_cover.82160bf0.png',
+    cover: require('@/assets/catagory.png'),
   },
   {
     id: 4,
     name: '教案',
     alias: 'essonplan',
-    avatar: 'http://localhost:8080/img/res_cover.82160bf0.png',
+    cover: require('@/assets/catagory.png'),
   },
   {
     id: 5,
     name: '视频',
     alias: 'video',
-    avatar: 'http://localhost:8080/img/res_cover.82160bf0.png',
+    cover: require('@/assets/catagory.png'),
   },
   {
     id: 6,
     name: '其他资源',
     alias: 'other',
-    avatar: 'http://localhost:8080/img/res_cover.82160bf0.png',
+    cover: require('@/assets/catagory.png'),
   },
 ];
 export default defineComponent({
