be46fc5220772eab55d73fc462df7298140f4e4b
rockts <rockts@sina.com>
2021-02-22 04:29:01 +0800

    开启拦截器

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -6,33 +6,33 @@ import { API_BASE_URL } from './app.config';
  */
 
 export const apiHttpClient = axios.create({
-  baseURL: API_BASE_URL,
+ baseURL: API_BASE_URL,
 });
 
 /**
  * 请求拦截器
  */
-// axios.interceptors.request.use(
-//   (config) => {
-//     console.log('axios 请求拦截器', config);
+axios.interceptors.request.use(
+ (config) => {
+  console.log('axios 请求拦截器', config);
 
-//     return config;
-//   },
-//   (error) => {
-//     return Promise.reject(error);
-//   }
-// );
+  return config;
+ },
+ (error) => {
+  return Promise.reject(error);
+ }
+);
 
 /**
  * 响应拦截器
  */
-// axios.interceptors.response.use(
-//   (response) => {
-//     console.log('axios 响应拦截器', response);
+axios.interceptors.response.use(
+ (response) => {
+  console.log('axios 响应拦截器', response);
 
-//     return response;
-//   },
-//   (error) => {
-//     return Promise.reject(error);
-//   }
-// );
+  return response;
+ },
+ (error) => {
+  return Promise.reject(error);
+ }
+);
