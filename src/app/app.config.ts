d04e6938811ab3425ba8b1f9ee8949a2e0639927
rockts <rockts@sina.com>
2021-02-24 21:52:41 +0800

    删除相关自动部署

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,7 +7,3 @@ export const APP_NAME = process.env.VUE_APP_NAME;
  * 服务接口基本地址
  */
 export const API_BASE_URL = process.env.VUE_APP_API_BASE_URL;
-
-module.exports = {
- publicPath: process.env.NODE_ENV === 'production' ? '/lejiaoku-vue/' : '/',
-};
