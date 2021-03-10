675587b247c353d9b351757c9bf52e5cd3295ce7
gaopeng <gaopeng@192.168.0.116>
2021-03-10 16:19:29 +0800

    无法运行

--- src/classifications/index/components/ClassificationsList.vue
+++ src/classifications/index/components/ClassificationsList.vue
@@ -1,47 +1,47 @@
 <template>
- <div class="category-list mb-3">
-  <div class="container">
-   <div v-if="loading">加载中...</div>
-   <div class="row">
-    <CategoryListItem
-     v-for="category in categorys"
-     :key="category.id"
-     :item="category"
-     class="col-md-4 py-3 px-5"
-    />
-   </div>
+  <div class="classifications-list mb-3">
+    <div class="container">
+      <div v-if="loading">加载中...</div>
+      <div class="row">
+        <ClassificationsListItem
+          v-for="classification in classifications"
+          :key="classification.id"
+          :item="classification"
+          class="col-md-4 py-3 px-5"
+        />
+      </div>
+    </div>
   </div>
- </div>
 </template>
 
 <script>
- import { defineComponent } from 'vue';
- import { mapGetters, mapActions } from 'vuex';
- import CategoryListItem from './category-list-item.vue';
+import { defineComponent } from "vue";
+import { mapGetters, mapActions } from "vuex";
+import ClassificationsListItem from "./ClassificationsListItem.vue";
 
- export default defineComponent({
-  name: 'CategoryList',
+export default defineComponent({
+  name: "ClassificationsList",
 
   async created() {
-   // 获取类型列表数据
-   this.getCategorys();
+    // 获取类型列表数据
+    this.getClassifications();
   },
 
   computed: {
-   ...mapGetters({
-    loading: 'category/index/loading',
-    categorys: 'category/index/categorys',
-   }),
+    ...mapGetters({
+      loading: "classifications/index/loading",
+      classifications: "classifications/index/classifications",
+    }),
   },
 
   methods: {
-   ...mapActions({
-    getCategorys: 'category/index/getCategorys',
-   }),
+    ...mapActions({
+      getClassifications: "classifications/index/getClassifications",
+    }),
   },
 
   components: {
-   CategoryListItem,
+    ClassificationsListItem,
   },
- });
+});
 </script>
