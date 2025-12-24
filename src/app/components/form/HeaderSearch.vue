c74fe4451851f4a96bf6b1fd02ade83d84ce7f12
rockts <gaopeng@lekee.cc>
2025-12-24 15:58:36 +0800

    fix: restore additional UI components from latest commit

--- src/app/components/form/HeaderSearch.vue
+++ src/app/components/form/HeaderSearch.vue
@@ -1,20 +1,61 @@
 <template>
- <form class="form-inline my-2 my-lg-0 d-sm-block d-md-none">
-  <input
-   class="form-control mr-sm-2"
-   type="search"
-   placeholder="Search"
-   aria-label="Search"
-  />
-  <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
-   搜索
-  </button>
- </form>
+  <form
+    class="form-inline my-2 my-lg-0 header-search-form"
+    @submit.prevent="onSearch"
+  >
+    <div class="input-group">
+      <input
+        class="form-control"
+        type="search"
+        placeholder="搜索资源..."
+        aria-label="Search"
+        v-model.trim="keyword"
+      />
+      <div class="input-group-append">
+        <button class="btn btn-outline-secondary" type="submit">
+          <i class="bi bi-search"></i>
+        </button>
+      </div>
+    </div>
+  </form>
 </template>
 
 <script>
- import { defineComponent } from 'vue';
- export default defineComponent({
-  name: 'HeaderSearch',
- });
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "HeaderSearch",
+  data() {
+    return {
+      keyword: "",
+    };
+  },
+  methods: {
+    onSearch() {
+      if (this.keyword) {
+        this.$router.push({ path: "/", query: { q: this.keyword } });
+      }
+    },
+  },
+});
 </script>
+
+<style scoped>
+.header-search-form {
+  margin-right: 15px;
+}
+.header-search-form .form-control {
+  border-radius: 20px 0 0 20px;
+  border-right: none;
+  width: 200px;
+  font-size: 14px;
+}
+.header-search-form .btn {
+  border-radius: 0 20px 20px 0;
+  border-left: none;
+  border-color: #ced4da;
+}
+.header-search-form .btn:hover {
+  background-color: #f8f9fa;
+  color: #333;
+}
+</style>
